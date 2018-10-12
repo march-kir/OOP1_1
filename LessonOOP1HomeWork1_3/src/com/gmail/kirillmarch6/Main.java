21a7b703a40b9fdd6567e28f602618fa372ee477
march-kir <kirillmarch6@gmail.com>
2018-10-12 11:51:33 +0300

    version2.0

--- LessonOOP1HomeWork1_3/src/com/gmail/kirillmarch6/Main.java
+++ LessonOOP1HomeWork1_3/src/com/gmail/kirillmarch6/Main.java
@@ -1,16 +1,28 @@
+//перевод числа из бинарной системы в десятичную
+
 package com.gmail.kirillmarch6;
+
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Vector3d a = new Vector3d(5, -1, 3);
-		Vector3d b = new Vector3d(1, 3, -4);
-		
-		a.summVectors3d(b);
-		a.productScalarVectorsd(b);
-		a.productVecors3d(b);
+		getNumberInDecimalFormat();
+	}
 
+	/**
+	 * Get number in Decimal format
+	 * 
+	 */
+	public static void getNumberInDecimalFormat() {
+		System.out.println("Input number in binary format");
+		Scanner sc = new Scanner(System.in);
+		try {
+			int number = sc.nextInt(2);
+			System.out.println("The number in decimal format - " + number);
+		} catch (Exception e) {
+			System.out.println("The number is not in binary format");
+		}
 	}
 
 }
